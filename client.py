cadc1c360fbf96fa6446e7d2df84d2acda678f91
Owen Foster <118078548+Torfmuer@users.noreply.github.com>
2023-12-11 21:59:23 -0800

    Uploaded the project!
    Added in the client and server files for the project, as well as the images necessary to send back to the client from the server!

---
+++ client.py
@@ -0,0 +1,55 @@
+### BINGUS DATABASE ###
+### NETWORKS FINAL ###
+### BY HUNTER WILLIAMS AND OWEN FOSTER ###
+
+import socket
+
+# Main function, send 
+def grab_image(ip):
+    # Initialize 
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = ip
+    port = 12345
+
+    # Connect to server
+    client_socket.connect((host, port))
+
+    # Receive greeting and send image option
+    print(client_socket.recv(1024).decode('utf-8'))
+    print('input image option: ')
+    img_op = input()
+    client_socket.send(img_op.encode('utf-8'))
+    
+    # Receive image size first
+    image_size_bytes = client_socket.recv(4)
+    image_size = int.from_bytes(image_size_bytes, byteorder='big')
+
+    # Receive image data
+    image_data = b''
+
+    while len(image_data) < image_size:
+        packet = client_socket.recv(image_size - len(image_data))
+        if not packet:
+            break
+        image_data += packet
+
+    # Write received image data to file
+    with open('received_bingus.jpg', 'wb') as file:
+        file.write(image_data)
+
+    print("~Image received and saved~")
+
+    # Close connection with server
+    client_socket.close()
+
+# MAIN
+print("~~~Bingus Database client app~~~\nq - Quit c - connect\n")
+while True:
+    client_in = input()
+    if client_in == 'q':
+        break
+    elif client_in == 'c':
+        grab_image('127.0.0.1') #### EDIT THIS STRING WITH CORRECT IP ####
+        print("~ Anything else? (q to quit) (c to connect again!)~")
+    else:
+        print("~~~InVaLId coMmAnD~~~")
---
+++ server.py
@@ -0,0 +1,51 @@
+### BINGUS DATABASE ###
+### NETWORKS FINAL ###
+### BY HUNTER WILLIAMS AND OWEN FOSTER ###
+
+import socket
+
+### Image option ###
+def img_op(client_socket, img_option):
+    with open(img_option + '.jpg', 'rb') as file:
+        image_data = file.read()
+
+        # Send image size
+        image_size = len(image_data)
+        client_socket.sendall(image_size.to_bytes(4, byteorder='big'))
+
+        # Send image data
+        client_socket.sendall(image_data)
+
+        # Close connection with client
+        client_socket.close()
+        
+# Create socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Get local machine name and port
+# May need some modifications for use on LAN
+host = '127.0.0.1'
+port = 12345
+
+# Bind to port
+server_socket.bind((host, port))
+
+# Listen for incoming connections
+server_socket.listen(5)
+
+print("Server listening on {}:{}".format(host, port))
+
+# Waiting for connections
+while True:
+    client_socket, addr = server_socket.accept()
+
+    print("Got a connection from {}".format(addr))
+
+    # Greeting
+    serv_greet = "Hello welcome to Bingus Database, here you may download any and all Binguses that you want? \n Which bingus would you like? \n 1. Standard bingus {bingus} \n 2. Splorngus {splorngus} \n 3. Suit bingus {suit} \n 4. Bingus with belly full {fullbingus} \n 5. Dripped out bingus {drip} \n"
+    client_socket.sendall(serv_greet.encode('utf-8'))
+    
+    # Grab the image option and send
+    img_option = client_socket.recv(1024)
+    img_op(client_socket,img_option.decode('utf-8'))
+        
